fc27cfe910458722beadb916400fd23b28825ee6
Ondřej Váňa <vanacraft@gmail.com>
2026-03-11 11:39:23 +0100

    Create Data analysis.sql

---
+++ Data analysis.sql
@@ -0,0 +1,25 @@
+-- Top 5 products by total sales per category
+
+-- Using a CTE to avoid aggregating total_sales inside the window function.
+WITH product_sales AS (
+	SELECT p.category,
+				 p.product_name,
+				 SUM(o.sales) AS product_total_sales
+	FROM orders AS o
+	JOIN products AS p USING(product_id)
+	GROUP BY p.category, p.product_name
+)
+SELECT category,
+			 product_name,
+			 product_total_sales,
+-- With ROW_NUMBER(), I will select exactly 5 products for each category.
+-- Whereas with RANK(), more than 5 products could be selected 
+-- if they share the same rank.
+			 ROW_NUMBER() OVER (
+				 PARTITION BY category
+				 ORDER BY product_total_sales DESC
+			 ) AS product_rank
+FROM product_sales
+-- Filtering only products ranked in the top 5.
+WHERE product_rank <= 5
+ORDER BY category, product_rank;
