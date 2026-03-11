b56637223f370f4951395f6da64917418af235e7
Ondřej Váňa <vanacraft@gmail.com>
2026-03-11 17:29:02 +0100

    Update Data cleaning.sql

--- Data cleaning.sql
+++ Data cleaning.sql
@@ -64,21 +64,50 @@ FROM orders;
 
 -- 5. Combining the steps into one query
 
+-- Step 1: Cleaning the data.
 WITH cleaned_data AS (
-	-- Step 1: Data cleaning.
+	SELECT order_id,
+			order_date,
+			customer_id,
+	   		UPPER(market) AS market,
+	   		INITCAP(region) AS region,
+			product_id,
+			sales,
+			quantity,
+	   		CASE
+	   	 		 WHEN discount < 0 THEN 0
+		  		WHEN discount > 1 THEN 1
+		  		ELSE discount
+	   		END AS discount
 	FROM orders
 ),
 duplicates_removed AS (
 	SELECT *
+-- Step 2: Identifying duplicate records.
 	FROM (
-		-- Step 2: Detecting duplicates.
+		SELECT *,
+				ROW_NUMBER() OVER (
+					PARTITION BY order_date, customer_id, product_id
+					ORDER BY order_id
+				) AS duplicate_rank
 		FROM cleaned_data
-	) AS duplicates_ranked
-	-- Step 3: Filtering out the duplicates.
+	) AS duplicates
+-- Step 3: Filtering out the duplicates.
+-- Note that a subquery had to be used in order to allow for filtering
+-- the results of a window function in the WHERE clause.
+-- This could be solved without a subquery using the QUALIFY clause in some engines
+-- (unavailable in PostgreSQL).
 	WHERE duplicate_rank = 1
 ),
+-- Step 4: Flagging data quality.
 validated_data AS (
-	-- Step 4: Flagging data quality.
+	SELECT *,
+			CASE
+				WHEN quantity IS NULL THEN 'missing quantity'
+				WHEN quantity <= 0 THEN 'invalid quantity'
+				WHEN sales < 0 THEN 'negative sales'
+				ELSE 'valid'
+			END AS data_quality_flag
 	FROM duplicates_removed
 )
 -- Step 5: Selecting only clean records.
